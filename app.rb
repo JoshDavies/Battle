3cdc34d6eb24bb628f9c779305deef8f820d76ab
Dom Tunstill <datunstill@gmail.com>
2019-10-02 17:32:21 +0100

    Added attack functionality

--- app.rb
+++ app.rb
@@ -3,20 +3,33 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
   enable :sessions
 
+
   get '/' do
+    session[:player1_hp] = 200
+    session[:player2_hp] = 200
     erb(:index)
   end
 
   post '/names' do
     session[:player1] = params[:player1]
     session[:player2] = params[:player2]
+    session[:header] = "#{session[:player1]}'s turn"
     redirect '/play'
   end
 
   get '/play' do
     @player1 = session[:player1]
     @player2 = session[:player2]
+    @player1_HP = session[:player1_hp]
+    @player2_HP = session[:player2_hp]
+    @header = session[:header]
     erb(:play)
+  end
+
+  get '/attac_player2' do
+    session[:player2_hp] -= 10
+    session[:header] = "#{session[:player1]} attacks #{session[:player2]}"
+    redirect '/play'
   end
 
   # start server if ruby file executed directly.
--- spec/feature/Player_hp_spec.rb
+++ spec/feature/Player_hp_spec.rb
@@ -1,8 +1,14 @@
 feature 'players can view their HP' do
 
-  scenario "p1 can seed p2's hp" do
+  scenario "p1 can see p2's hp" do
     sign_in_and_play
     expect(page).to have_content "Josh the destroyer: 200hp The DOMinator: 200hp"
+  end
+
+  scenario "p1 can attack p2's hp" do
+    sign_in_and_play
+    click_button('attac!')
+    expect(page).to have_content "Josh the destroyer: 200hp The DOMinator: 190hp"
   end
 
 end
