c18b984bc20c1391e8abb01d7000460676bfab47
Student <student@Makerss-MacBook-Pro.local>
2019-10-02 16:41:50 +0100

    refactored

--- spec/feature/Player_hp_spec.rb
+++ spec/feature/Player_hp_spec.rb
@@ -1,7 +1,7 @@
 feature 'players can view their HP' do
 
   scenario "p1 can seed p2's hp" do
-
+    sign_in_and_play
     expect(page).to have_content "Josh the destroyer: 200hp The DOMinator: 200hp"
   end
 
